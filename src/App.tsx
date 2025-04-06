a85c6164f81c1eb7dbef91f6faf3bf3a61877ca9
Ramonprog <programador.ramon@gmail.com>
2025-04-06 17:49:19 -0300

    priimeiro commit 🎉

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "antd"
+
+function App() {
+
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Meu Projeto com Vite + React + Ant Design</h1>
+      <Button type="primary">Botão Ant Design</Button>
+    </div>
+  )
+}
+
+export default App
